51b8773c3c69350922b2ce156bc561c2c7745efa
Matheus HEnry <Matheus HEnry@MatheusHEnry-PC>
2018-06-07 21:23:30 -0400

    segundo commit

--- src/semanadomeioambiente/Semanadomeioambiente.java
+++ src/semanadomeioambiente/Semanadomeioambiente.java
@@ -15,7 +15,7 @@ public class Semanadomeioambiente {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ola mundo!!!");
+        System.out.println("Ola mundo");
         System.out.println("Semana do meio ambiente");
         System.out.println("2018");
     }
