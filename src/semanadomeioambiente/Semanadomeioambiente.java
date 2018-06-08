87a673032f4eaa64c3c2448073b492e3a99ebceb
Matheus HEnry <Matheus HEnry@MatheusHEnry-PC>
2018-06-07 21:20:51 -0400

    primeiro commit

---
+++ src/semanadomeioambiente/Semanadomeioambiente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semanadomeioambiente;
+
+/**
+ *
+ * @author Matheus HEnry
+ */
+public class Semanadomeioambiente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ola mundo!!!");
+        System.out.println("Semana do meio ambiente");
+        System.out.println("2018");
+    }
+    
+}
